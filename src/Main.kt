c3feeb628437d4472f7750ecc6c74dd2846769e0
Ace Alrich <hex0@live.com>
2020-05-07 18:47:27 +0100

    Custom getters and setters

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,7 @@ var names = arrayOf("Ace","Ilona")
 fun sayHello(greeting: String = "Hello", name: String ) = print("$greeting $name\n")
 fun main() {
 //    names.forEach { it: String -> sayHello(name = it) }
-    val person = Person("Ace","Aldrich")
+    val person = Person()
     person.lastName
+    person.nickName = "Lala"
 }
--- src/Person.kt
+++ src/Person.kt
@@ -1,1 +1,11 @@
-class Person(val firstName: String,val lastName: String) {}
+class Person(val firstName: String = "Ilona",val lastName: String = "Aldrich") {
+    var nickName: String = "Lalala"
+    set(value){
+        field = value
+        println("the new nickname is $value")
+    }
+    get(){
+        println("the return value is $field")
+        return field
+    }
+}
