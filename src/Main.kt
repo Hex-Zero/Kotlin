e54b6a122eb910411ff44e5b5893a5ad15306335
Ace Alrich <hex0@live.com>
2020-05-07 18:04:39 +0100

    Single line function expresion

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,7 @@
 
 
 var name = "Ace"
-
+fun sayHello( name: String ) = print("Hello $name")
 fun main() {
-    when(name){
-        "Ace" -> println("Ace Aldrich")
-        else -> println("Someone else")
-    }
+    sayHello(name)
 }
