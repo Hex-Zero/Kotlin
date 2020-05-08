282a2f6017fd2557175f6ee843750e31f1d00b59
Ace Alrich <hex0@live.com>
2020-05-08 13:50:18 +0100

    Interfaces check types

--- src/PersonInfoProvider.kt
+++ src/PersonInfoProvider.kt
@@ -5,14 +5,32 @@ interface PersonInfoProvider {
         person.printInfo()
     }
 }
-class BasicProvider(override val providerInfo: String = "BasicInfoProvider") : PersonInfoProvider {
+interface SessionInfoProvider{
+    fun getSessionId() : String
+}
+class BasicProvider(override val providerInfo: String = "BasicInfoProvider") : PersonInfoProvider , SessionInfoProvider{
     override fun printInfo(person: Person) {
         super.printInfo(person)
         println("additional print statement")
+    }
+
+    override fun getSessionId(): String {
+        return "Session"
     }
 }
 fun main(){
     val provider = BasicProvider()
 
     provider.printInfo(Person())
+    provider.getSessionId()
+
+    checkTypes(provider)
+}
+
+fun checkTypes(infoProvider: PersonInfoProvider){
+    if(infoProvider is SessionInfoProvider){
+        println("is a session info provider")
+    }else{
+        println("not a session info provider")
+    }
 }
